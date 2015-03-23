a7049897ed7a6696084dbb836e0fa3ac629d2c28
SuigetsuSake <mikael.sanchez1@gmail.com>
2015-03-23 02:19:57 +0100

    Fichier manquant

---
+++ MP22NET/MP22NET/Pages/Admin/EmployeesViewModel.cs
@@ -0,0 +1,40 @@
+﻿using FirstFloor.ModernUI.Presentation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MP22NET.Pages.Admin
+{
+    
+    class EmployeesViewModel 
+        : NotifyPropertyChanged
+    {
+        public string edition;
+        public Employee selectedEmployee;
+        
+
+        public EmployeesViewModel()
+        {
+            edition = "Visible";
+        }
+
+        public string editionVisibility
+        {
+            get { return this.edition; }
+            set { this.edition = value; }
+        }
+
+        public Employee SelectedEmployee
+        {
+            get { return this.selectedEmployee; }
+            set { 
+                this.selectedEmployee = value;
+                editionVisibility = "Hidden";
+            }
+        }
+    }
+}
